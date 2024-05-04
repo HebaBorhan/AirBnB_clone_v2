86363805a66337a80268300f4df348f0b001c6e8
HebaZaki <h_borhan@hotmail.com>
2024-05-04 13:48:54 +0300

    archiving

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,11 +6,11 @@ from datetime import datetime
 
 def do_pack():
     """Compress the contents of web_static folder into a .tgz archive"""
-    time = datetime.now()
-    archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
-    c.run('mkdir -p versions')
-    create = c.local('tar -cvzf versions/{} web_static'.format(archive))
-    if create.failed:
-        return None
-    else:
+    current = datetime.now()
+    archive = 'web_static_' + current.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
+    local('mkdir -p versions')
+    create = local('tar -cvzf versions/{} web_static'.format(archive))
+    if create is not None:
         return archive
+    else:
+        return None
