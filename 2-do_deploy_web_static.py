22aea5d885d57d8a9fe124e60434f7e2c9b115a2
HebaZaki <h_borhan@hotmail.com>
2024-05-04 15:44:39 +0300

    Distributing archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,9 +2,8 @@
 """Fabric script that distributes an archive to your web servers"""
 from fabric.api import *
 from os import path
-
-
 env.hosts = ['23.23.73.165', '54.144.141.225']
+
 
 def do_deploy(archive_path):
     """Distributing an archive to my web servers"""
@@ -13,7 +12,8 @@ def do_deploy(archive_path):
 
     try:
         archive_name = archive_path.split('/')[-1]
-        archive_folder = '/data/web_static/releases/{}'.format(archive_name[:-4])
+        archive_folder = '/data/web_static/releases/{}'.format(
+            archive_name[:-4])
         put(archive_path, '/tmp/')
         run('mkdir -p {}'.format(archive_folder))
         run('tar -xzf /tmp/{} -C {}'.format(archive_name, archive_folder))
@@ -23,5 +23,5 @@ def do_deploy(archive_path):
         run('rm -rf /data/web_static/current')
         run('ln -s {} /data/web_static/current'.format(archive_folder))
         return True
-    except:
+    except Exception as e:
         return False
