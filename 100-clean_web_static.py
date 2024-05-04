cd2cc7313ac2acedc9b448faad049ff8d49aaac4
HebaZaki <h_borhan@hotmail.com>
2024-05-04 21:42:14 +0300

    clean

---
+++ 100-clean_web_static.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Fabric script that deletes out-of-date archives"""
+from fabric.api import *
+from os.path import isdir
+env.hosts = ['23.23.73.165', '54.144.141.225']
+
+
+def do_clean(number=0):
+    """Deletes out-of-date archives"""
+    try:
+        number = int(number)
+        if number < 1:
+            number = 1
+
+        # Delete unnecessary archives in versions folder
+        with lcd('versions'):
+            local('ls -t | tail -n +{} | xargs rm -f'.format(number + 1))
+
+        # Delete unnecessary archives in /data/web_static/releases folder
+        with cd('/data/web_static/releases'):
+            run('ls -t | tail -n +{} | xargs rm -rf'.format(number + 1))
+
+        return True
+    except Exception as e:
+        return False
