c3a28d2be8adf9ed66e7589d59cdd6b42e9b04cf
HebaZaki <h_borhan@hotmail.com>
2024-05-04 12:32:28 +0300

    setting up erb static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# This script sets up my web servers for the deployment of web_static
+
+sudo apt-get update
+sudo apt-get -y install nginx
+
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+
+sudo touch /data/web_static/releases/test/index.html
+echo "<html>
+<head>
+<title>Test</title>
+</head>
+<body>
+<h1>This is Heba Zaki's test page</h1>
+</body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+sudo chown -R ubuntu:ubuntu /data/
+
+sudo sed -i '/listen 80 default_server;/a \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+
+sudo service nginx restart
