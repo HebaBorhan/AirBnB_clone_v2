1a1dc12b56ac7d58d87de34a4af3dc83fe4ec2f2
HebaZaki <h_borhan@hotmail.com>
2024-05-04 13:38:07 +0300

    archiving

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,30 +1,16 @@
 #!/usr/bin/python3
 """Fabric script for archiving"""
-from fabric.api import local
+from fabric.api import *
 from datetime import datetime
 import os
 
 def do_pack():
     """Compress the contents of web_static folder into a .tgz archive"""
-    try:
-        # Create the 'versions' directory if it doesn't exist
-        if not os.path.exists("versions"):
-            os.makedirs("versions")
-
-        # Generate the current date and time for the archive name
-        now = datetime.now()
-        timestamp = now.strftime("%Y%m%d%H%M%S")
-
-        # Create the archive name
-        archive_name = "web_static_" + timestamp + ".tgz"
-
-        # Compress the contents of the web_static folder into a .tgz archive
-        local("tar -czvf versions/{} web_static".format(archive_name))
-
-        # Return the archive path if the archive has been correctly generated
-        return os.path.join("versions", archive_name)
-
-    except Exception as e:
-        # Print an error message if an exception occurs
-        print("Error:", e)
+    time = datetime.now()
+    archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
+    local('mkdir -p versions')
+    create = local('tar -cvzf versions/{} web_static'.format(archive))
+    if create is not None:
+        return archive
+    else:
         return None
