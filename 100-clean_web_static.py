d6dec847343f21c35dbfca6f09f9e2e8ed48c717
HebaZaki <h_borhan@hotmail.com>
2024-05-06 22:50:32 +0300

    clean

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
 """Fabric script that deletes out-of-date archives"""
 from fabric.api import *
-import os
+from os.path import isdir
 env.hosts = ['23.23.73.165', '54.144.141.225']
 
 
 def do_clean(number=0):
     """Deleting out-of-date archives"""
-    number = 1 if int(number) == 0 else int(number)
+    try:
+        number = int(number)
+        if number <= 0:
+            number = 1
 
-    archives = sorted(os.listdir("versions"))
-    [archives.pop() for i in range(number)]
-    with lcd("versions"):
-        [local("rm ./{}".format(a)) for a in archives]
+        with lcd('versions'):
+            local('ls -t | tail -n +{} | xargs rm -f'.format(number + 1))
 
-    with cd("/data/web_static/releases"):
-        archives = run("ls -tr").split()
-        archives = [a for a in archives if "web_static_" in a]
-        [archives.pop() for i in range(number)]
-        [run("rm -rf ./{}".format(a)) for a in archives]
+        with cd('/data/web_static/releases'):
+            run('ls -t | tail -n +{} | xargs rm -rf'.format(number + 1))
+
+        return True
+    except Exception as e:
+        return False
