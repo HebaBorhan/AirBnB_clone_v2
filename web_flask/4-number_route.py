8efb913a37a7ae959ff03da3c1a122241f1ab494
HebaZaki <h_borhan@hotmail.com>
2024-05-18 10:59:50 +0300

    pynumber flask

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Flask web app"""
+from flask import Flask
+
+
+web_app = Flask(__name__)
+
+
+@web_app.route("/", strict_slashes=False)
+def hellopage():
+    """hello page"""
+    return "Hello HBNB!"
+
+
+@web_app.route("/hbnb", strict_slashes=False)
+def hbnbpage():
+    """hbnb page"""
+    return "HBNB"
+
+
+@web_app.route("/c/<text>", strict_slashes=False)
+def Cpage(text):
+    """C page"""
+    return "C " + text.replace("_", " ")
+
+
+@web_app.route("/python/", defaults={'text': 'is cool'}, strict_slashes=False)
+@web_app.route("/python/<text>", strict_slashes=False)
+def Pythonpage(text):
+    """Python page"""
+    return "Python " + text.replace("_", " ")
+
+
+@web_app.route("/number/<int:n>", strict_slashes=False)
+def intpage(n):
+    """integer page"""
+    return "{:d} is a number".format(n)
+
+
+if __name__ == "__main__":
+    web_app.run(host="0.0.0.0", port=5000)
