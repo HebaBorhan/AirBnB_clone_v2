f3c207f51315bf38318dc21a0fee6b51a915606d
HebaZaki <h_borhan@hotmail.com>
2024-05-19 12:12:47 +0300

    update file

--- models/city.py
+++ models/city.py
@@ -11,9 +11,15 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-    places = relationship("Place", cascade="delete", backref="cities")
+    places = relationship("Place", cascade="all, delete", backref="cities")
 
     if os.getenv("HBNB_TYPE_STORAGE") != "db":
-        def __init__(self,*args,**kwargs):
-            """New City instance"""
-            super().__init__(**kwargs)
+        def places(self):
+            """getter attribute for places"""
+            from models import storage
+            all_places = storage.all("Place")
+            return [place for place in all_places.values() if place.city_id == self.id]
+        
+    def __init__(self,*args,**kwargs):
+        """New City instance"""
+        super().__init__(*args,**kwargs)
