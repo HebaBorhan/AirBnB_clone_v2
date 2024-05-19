0619ce28c239acf5aa625d0a4637115c667941bf
HebaZaki <h_borhan@hotmail.com>
2024-05-19 23:52:47 +0300

    states of state

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -12,17 +12,20 @@ web_app = Flask(__name__)
 @web_app.route("/states", strict_slashes=False)
 def mystates():
     """listing  all State objects"""
-    sorted_states = storage.all(State).values()
-    return render_template('9-states.html', states=sorted_states)
+    states = storage.all(State)
+    sorted_states = sorted(states.values(), key=lambda state: state.name)
+    return render_template('9-states.html', states=sorted_states, state=None)
 
 
 @web_app.route("/states/<id>", strict_slashes=False)
-def statesid():
+def statesid(state_id):
     """listing cities of a specific State"""
-    state = storage.all(State).get(f'State.{id}')
+    state_key = 'State.' + state_id
+    states = storage.all(State)
+    state = states.get(state_key)
     if state is None:
-        abort(404)
-    return render_template('9-states.html', state=state)
+        return render_template('9-states.html', states=[], state=None, not_found=True)
+    return render_template('9-states.html', states=[], state=state, not_found=False)
 
 
 @web_app.teardown_appcontext
