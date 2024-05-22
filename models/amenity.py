dadae7a9dcb99d63edebf6fe406612c1e4ed251c
HebaZaki <h_borhan@hotmail.com>
2024-05-22 23:07:13 +0300

    amenity file

--- models/amenity.py
+++ models/amenity.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import os
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
@@ -7,9 +8,13 @@ from sqlalchemy.orm import relationship
 
 class Amenity(BaseModel, Base):
     """Class defining Amenity"""
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary="place_amenity", viewonly=False)
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = "amenities"
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship(
+            "Place", secondary="place_amenity", viewonly=False)
+    else:
+        name = ""
 
     def __init__(self,*args,**kwargs):
         """New Amenity instance"""
